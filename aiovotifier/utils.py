cd9815051009281cd63e6a630cb5f0ea0023da81
Thaodan <unclethaodan@gmail.com>
2023-01-04 11:11:47 +0100

    Add support for alternate rsa header
    Allows for use in conjunction with python rsa library (https://github.com/sybrenstuvel/python-rsa)

--- aiovotifier/utils.py
+++ aiovotifier/utils.py
@@ -3,7 +3,7 @@ def ensure_pem_format(key: str) -> str:
 
     key = key.strip()
 
-    if not key.startswith("-----BEGIN PUBLIC KEY-----"):
+    if not key.startswith("-----BEGIN PUBLIC KEY-----") and not key.startswith("-----BEGIN PUBLIC RSA KEY-----"):
         key = key.replace("\r", "").replace("\n", "")
         key = "\n".join([key[i : i + 64] for i in range(0, len(key), 64)])
         key = "-----BEGIN PUBLIC KEY-----\n" + key + "\n-----END PUBLIC KEY-----\n"
